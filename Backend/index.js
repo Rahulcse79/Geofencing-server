62e573d3d08c6b4cfdf6f8c95f3f3cb108158469
Rahul singh <100202443+Rahulcse79@users.noreply.github.com>
2025-03-28 22:16:46 +0530

    view

--- Backend/index.js
+++ Backend/index.js
@@ -288,6 +288,15 @@ const generateRandomOTP = () => {
   return Math.floor(100000 + Math.random() * 900000).toString();
 };
 
+app.get('/api/view', async (req, res) => {
+  try {
+    return res.status(200).json({ message: "view successful", status: 1 });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error', error: error.message, status: -1 });
+  }
+});
+
+
 // Api of user login.
 app.post('/api/login', async (req, res) => {
   try {
